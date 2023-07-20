4e001961f088a884db280d9bd7a471db41451583
sanasainath <127873136+sanasainath@users.noreply.github.com>
2023-07-20 23:27:06 +0530

    Add files via upload

---
+++ insert a node at single linked list.java
@@ -0,0 +1,44 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = next;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node insertAtFirst(Node list, int newValue) {
+        // Write your code here
+    Node newnode=new Node(newValue);
+    Node head=list;
+    newnode.next=head;
+    head=newnode;
+    return head;
+    }
+}
