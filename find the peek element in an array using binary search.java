ec4dcd923b89bc4c9e9caec839cc9fa6dde0448f
sanasainath <127873136+sanasainath@users.noreply.github.com>
2023-07-28 23:43:07 +0530

    Add files via upload

---
+++ find the peek element in an array using binary search.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length==1)
+        {
+            return 0;
+        }
+        int n=nums.length;
+        if(nums[0]>nums[1])
+        {
+            return 0;
+        }
+        if(nums[n-1]>nums[n-2])
+        {
+            return n-1;
+        }
+        int low=1;int high=n-2;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(nums[mid]>nums[mid-1]&&nums[mid]>nums[mid+1])
+            {
+                return mid;
+            }
+            else if(nums[mid]>nums[mid-1])
+            {
+                low=mid+1;
+            }
+            else
+            {
+            high=mid-1;
+        }
+        }
+        return -1;
+        
+    }
+}
