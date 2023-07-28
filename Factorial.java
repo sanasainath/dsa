70ecbbe56b28cbacdd4deb7332298d3dc9ff0184
sanasainath <127873136+sanasainath@users.noreply.github.com>
2023-07-28 23:50:52 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+    public static void main(String[] args)
+    {
+        int i=1;
+        int n=5;
+        while(n>0)
+        {
+            i=n*i;
+            n--;
+        }
+        System.out.println(i);
+    }
+    
+}
