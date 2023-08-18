32b2604a85d8ed63be70fd5e7132e968a9572208
sanasainath <127873136+sanasainath@users.noreply.github.com>
2023-08-18 22:19:51 +0530

    Add files via upload

---
+++ longest commom prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder result=new StringBuilder();
+        Arrays.sort(strs);
+        char[] first=strs[0].toCharArray();
+        char[] second=strs[strs.length-1].toCharArray();
+        for(int i=0;i<first.length;i++)
+        {
+            if(first[i]!=second[i])
+            {
+                break;
+            }
+            result.append(first[i]);
+        }
+        return result.toString();
+    }
+}
