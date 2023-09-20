74fa454750b2c189223fa4a0a97240d9fd88ebd2
sanasainath <127873136+sanasainath@users.noreply.github.com>
2023-09-20 23:49:55 +0530

    Add files via upload

---
+++ swap two numbers.java
@@ -0,0 +1,11 @@
+public class Solution{
+    public static void swapNumber(int []a, int []b) {
+        // Write your code here.
+       for(int i=0;i<a.length&&i<b.length;i++)
+       {
+           int temp=a[i];
+           a[i]=b[i];
+           b[i]=temp;
+       }
+    }
+}
