5b768e943f2d4d8f9adec71f8f4e0becef45c0b3
sanasainath <127873136+sanasainath@users.noreply.github.com>
2023-10-05 01:31:48 +0530

    Add files via upload

---
+++ introduction to trees.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Solution {
+    public static int numberOfNodes(int N){
+        int a=2;
+        int n=(int)Math.pow(a,N-1);
+        return n;
+      
+    }
+}
