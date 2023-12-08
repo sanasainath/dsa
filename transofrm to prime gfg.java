8b5c43fd693ff461c99df7b3aa2ebce7dc429311
sanasainath <127873136+sanasainath@users.noreply.github.com>
2023-12-08 23:55:39 +0530

    Add files via upload

---
+++ transofrm to prime gfg.java
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcases = Integer.parseInt(br.readLine());
+        while (testcases-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(a1[i]);
+            }
+            Solution ob = new Solution();
+            int ans=ob.minNumber(a,n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+class Solution
+{
+    boolean[] primes;
+    int MAX;
+    Solution()
+    {
+        MAX=1000001;
+        primes=new boolean[MAX];
+        Arrays.fill(primes,true);
+        primes[1]=false;
+        
+        for(int i=2;i*i<MAX;i++)
+        {
+            if(primes[i]){
+                for(int j=i*2;j<MAX;j+=i)
+                {
+                    primes[j]=false;
+                }
+            }
+        }
+    }
+    public int minNumber(int arr[], int N)
+    {
+        int sum=0;
+        
+        for(int i=0;i<N;i++) sum+=arr[i];
+        
+        int st=sum;
+        
+        while(primes[st]==false)
+            st++;
+            
+        return st-sum;
+    }
+}
