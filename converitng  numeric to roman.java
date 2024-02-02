5c199bbb4813a4820575a5b75ead9024ffd93296
sanasainath <127873136+sanasainath@users.noreply.github.com>
2024-02-02 23:40:59 +0530

    Add files via upload

---
+++ converitng  numeric to roman.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.*;
+
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int N = sc.nextInt();
+			
+			Solution ob = new Solution();
+			System.out.println(ob.convertToRoman(N));
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+	String convertToRoman(int n) {
+		//code here
+	StringBuilder roman = new StringBuilder();
+
+        if (n < 1) {
+            return roman.toString();
+        }
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] numerals = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        for (int i = 0; i < values.length; i++) {
+            while (n >= values[i]) {
+                n -= values[i];
+                roman.append(numerals[i]);
+            }
+        }
+
+        return roman.toString();
+	}
+}
