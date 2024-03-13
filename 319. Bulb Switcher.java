2231af92ecaae38aa01f4a44cfd88fddbbb3f278
sanasainath <127873136+sanasainath@users.noreply.github.com>
2024-03-13 21:40:00 +0530

    Add files via upload

---
+++ 319. Bulb Switcher.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int res = 0;
+        for (int i = 1; i * i <= n; i++) {
+            res++;
+        }
+        return res;
+    }
+}
