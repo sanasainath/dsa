0fcdb7a31d5f3554fd7c9b06cdd5c0ec72385864
sanasainath <127873136+sanasainath@users.noreply.github.com>
2024-04-12 22:20:06 +0530

    Add files via upload

---
+++ Sum of Products pot of gfg.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            long Arr[] = new long[N];
+            String[] S = read.readLine().split(" ");
+            for (int i = 0; i < N; i++) Arr[i] = Long.parseLong(S[i]);
+            Solution ob = new Solution();
+            System.out.println(ob.pairAndSum(N, Arr));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static long pairAndSum(int n, long arr[]) {
+        long totalSum = 0;
+        long[] bitCount = new long[32]; // Assuming integers are 32-bit
+
+        for (int i = 0; i < 32; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if ((arr[j] & (1L << i)) != 0) {
+                    count++;
+                }
+            }
+            bitCount[i] = count;
+        }
+
+        for (int i = 0; i < 32; i++) {
+            totalSum += (1L << i) * bitCount[i] * (bitCount[i] - 1) / 2;
+        }
+
+        return totalSum;
+    }
+}
