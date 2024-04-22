0ba15191721db29f62ffe825d5cfa8f3e390d6d0
sanasainath <127873136+sanasainath@users.noreply.github.com>
2024-04-22 09:19:43 +0530

    Add files via upload

---
+++ Row with minimum number of 1's pot of gfg.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s[] = read.readLine().split(" ");
+            int N = Integer.parseInt(s[0]);
+            int M = Integer.parseInt(s[1]);
+            int A[][] = new int[N][M];
+            for (int i = 0; i < N; i++) {
+                String S[] = read.readLine().split(" ");
+                for (int j = 0; j < M; j++) {
+                    A[i][j] = Integer.parseInt(S[j]);
+                }
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.minRow(N, M, A));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int minRow(int n, int m, int a[][]) {
+        // code here
+        int maxi=0;
+        int check=Integer.MAX_VALUE;
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            for(int j=0;j<m;j++)
+            {
+                if(a[i][j]==1)
+                {
+                    sum++;
+                }
+            }
+            if(sum<check)
+            {
+              check=sum;
+               maxi=i;
+              
+            }
+           
+        }
+        return maxi+1;
+    }
+};
