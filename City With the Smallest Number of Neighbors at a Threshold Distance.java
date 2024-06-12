643c0e9a6de7472fd2c8c17275e5300cc0ad3b9b
sanasainath <127873136+sanasainath@users.noreply.github.com>
2024-06-12 10:00:54 +0530

    Update City With the Smallest Number of Neighbors at a Threshold Distance.java

--- City With the Smallest Number of Neighbors at a Threshold Distance.java
+++ City With the Smallest Number of Neighbors at a Threshold Distance.java
@@ -37,47 +37,58 @@ class Main {
 // User function Template for Java
 
 class Solution {
-    int findCity(int n, int m, int[][] edges, int distanceThreshold) {
-        int[][] grid = new int[n][n];
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j) {
-                    grid[i][j] = 1_0000_0000;
-                }
-            }
-        }
-        
-        for (int[] edge : edges) {
-            grid[edge[0]][edge[1]] = edge[2];
-            grid[edge[1]][edge[0]] = edge[2];
-        }
-        
-        for (int k = 0; k < n; k++) {
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    grid[i][j] = Math.min(grid[i][j], grid[i][k] + grid[k][j]);
-                }
-            }
-        }
-        
-        int mini = 1_0000_0000, city = -1;
-        
-        for (int i = 0; i < n; i++) {
-            int cnt = 0;
-            for (int j = 0; j < n; j++) {
-                if (i != j) {
-                    if (grid[i][j] <= distanceThreshold) {
-                        cnt++;
-                    }
-                }
-            }
-            if (cnt <= mini) {
-                mini = cnt;
-                city = i;
-            }
-        }
-        
-        return city;
-    }
+      int findCity(int n, int m, int[][] edges,int distanceThreshold)
+      {
+          //code here
+          
+          int [][]matrix=new int[n][n];
+          for(int []rows:matrix)
+          {
+              Arrays.fill(rows,Integer.MAX_VALUE);
+          }
+          for(int i=0;i<n;i++)
+          {
+              matrix[i][i]=0;
+              
+          }
+          for(int i=0;i<m;i++)
+          {
+              matrix[edges[i][0]][edges[i][1]]=edges[i][2];
+              matrix[edges[i][1]][edges[i][0]]=edges[i][2];
+          }
+          for(int i=0;i<n;i++)
+          {
+              for(int j=0;j<n;j++)
+              {
+                  for(int k=0;k<n;k++)
+                  {
+                      if(matrix[j][i]!=Integer.MAX_VALUE&&matrix[i][k]!=Integer.MAX_VALUE&&matrix[j][k]>matrix[j][i]+matrix[i][k])
+                      {
+                          matrix[j][k]=matrix[j][i]+matrix[i][k];
+                      }
+                  }
+              }
+          }
+          int countmin=Integer.MAX_VALUE;
+          int city=-1;
+          for(int i=0;i<n;i++)
+          {
+              int count=0;
+              for(int j=0;j<n;j++)
+              {
+                  if(matrix[i][j]<=distanceThreshold)
+                  {
+                      count++;
+                  }
+              }
+              if(count<countmin||(count==countmin&&i>city))
+              {
+                  city=i;
+                  countmin=count;
+                  
+              }
+          }
+          return city;
+          
+      }
 }
