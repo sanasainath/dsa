595d88f29a410e218c23a7094258c10cbc0df248
sanasainath <127873136+sanasainath@users.noreply.github.com>
2024-06-20 17:27:06 +0530

    Add files via upload

---
+++ 81. Search in Rotated Sorted Array II using binary search.java
@@ -0,0 +1,52 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+while(low<=high)
+{
+    int mid=(low+high)/2;
+    //left is sorted
+    if(nums[mid]==target)
+    {
+        return true;
+    }
+    else if(nums[mid]==nums[low]&&nums[mid]==nums[high])
+    {
+        low=low+1;
+        high=high-1;
+        continue;
+    }
+    else if(nums[low]<=nums[mid])
+    {
+        if(nums[low]<=target&&target<=nums[mid])
+        {
+            high=mid-1;
+
+
+
+        }
+        else
+        {
+            low=mid+1;
+        }
+    }
+    //right sorted
+    else
+    {
+        if(nums[mid]<=target&&target<=nums[high])
+             {
+                low=mid+1;
+             }
+             else
+             {
+                high=mid-1;
+             }
+
+
+    }
+}
+return false;
+
+        
+    }
+}
