332dfd9770442032aa207d210978cadbf2024a07
sanasainath <127873136+sanasainath@users.noreply.github.com>
2024-06-26 16:45:47 +0530

    Update 108. Convert Sorted Array to Binary Search Tree using bianry search and related to today pot of leetcode.java

--- 108. Convert Sorted Array to Binary Search Tree using bianry search and related to today pot of leetcode.java
+++ 108. Convert Sorted Array to Binary Search Tree using bianry search and related to today pot of leetcode.java
@@ -19,6 +19,12 @@ class Solution {
         }
         int mid = (int)Math.ceil((double)(low + high) / 2);
 
+        //  int mid = (int)Math.ceil((double)(low + high) / 2);
+        // prints [0,-3,9,-10,null,5]
+        // int mid=low+(high-low)/2;
+        //and int mid=low+(high-low)/2; this prints  [0,-10,5,null,-3,null,9]
+        //both are corrrect/////
+
         TreeNode node=new TreeNode(nums[mid]);
         node.left=path(nums,low,mid-1);
         node.right=path(nums,mid+1,high);
