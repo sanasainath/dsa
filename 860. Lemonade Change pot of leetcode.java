c99f8636a8296ed78a5ad9545fff9617d695a017
sanasainath <127873136+sanasainath@users.noreply.github.com>
2024-08-15 08:26:52 +0530

    Add files via upload

---
+++ 860. Lemonade Change pot of leetcode.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0;
+        int ten=0;
+        int twenty=0;
+        for(int i:bills)
+        {
+            if(i==5)
+            {
+                five++;
+            }
+            else if(i==10&&five>0)
+            {
+                five--;
+                ten++;
+            }
+            else 
+            {
+                if(ten>0&&five>0)
+                {
+                     ten--;
+                     five--;
+                }
+                else if(five>=3)
+                {
+                    five-=3;
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+}
