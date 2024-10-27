65b917f6c6d8f41d2673e686f9eac7c3d8b4edaa
sanasainath <127873136+sanasainath@users.noreply.github.com>
2024-10-27 14:54:18 +0530

    Add files via upload

---
+++ 3330. Find the Original Typed String I  yesterday contest leetcode biweekly.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int possibleStringCount(String word) {
+        int count=1;
+        for(int i=1;i<word.length();i++)
+        {
+            if(word.charAt(i-1)==word.charAt(i))
+            {
+                count++;
+            }
+        }
+        return count;
+        
+    }
+}
