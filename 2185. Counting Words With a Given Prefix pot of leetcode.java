016d24de4de9481b158fccd2af464e34116faba2
sanasainath <127873136+sanasainath@users.noreply.github.com>
2025-01-09 15:22:30 +0530

    Add files via upload

---
+++ 2185. Counting Words With a Given Prefix pot of leetcode.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+        int count=0;
+        for(String i:words)
+        {
+          if(i.startsWith(pref))
+          {
+            count++;
+          }
+        }
+        return count;
+        
+    }
+}
